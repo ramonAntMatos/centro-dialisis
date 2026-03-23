3686efd0b48a4701dae49154b1d93da6fcfe155a
Ramon Antonio Matos Marte <ramonantoniomatosmarte@Ramons-MacBook-Air.local>
2026-03-23 18:31:30 -0400

    fix: rutas correctas Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from './pages/Home';
+import Home from './pages/home';
 import Nosotros from "./pages/Nosotros/Nosotros";
 import Servicios from "./pages/Servicios/Servicios"; 
 import Informacion from "./pages/Informacion/Informacion"
